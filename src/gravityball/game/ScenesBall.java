ad00c40ffd5ec6c703e6c59ce758e2903e196903
Wenjie Zhang <zhang_a_a_a@outlook.com>
2017-05-17 12:57:53 +0800

    添加小球运动方程

--- src/gravityball/game/ScenesBall.java
+++ src/gravityball/game/ScenesBall.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Sphere;
@@ -33,8 +34,18 @@ public class ScenesBall {
 	/** 转动惯量 */
 	public float momentOfInertia;
 
+	/** 阻力系数 */
+	public float alpha_v;
+	/** 滚阻系数 */
+	public float alpha_m;
+
 	/** 半径 */
 	public float radius;
+
+	/** 中间变量，用于加速运算 */
+	private double under_f;
+	/** 中间变量，用于加速运算 */
+	private double under_m;
 
 	private Scenes scenes;
 
@@ -56,6 +67,8 @@ public class ScenesBall {
 		this.miu = (float) j.getDouble("miu");
 		this.momentOfInertia = (float) j.getDouble("momentOfInertia");
 		this.radius = (float) j.getDouble("radius");
+		this.alpha_m = (float) j.getDouble("alpha_m");
+		this.alpha_v = (float) j.getDouble("alpha_v");
 
 		// 初始化运动参数
 		this.velocityX = 0.f;
@@ -67,9 +80,69 @@ public class ScenesBall {
 		this.angular = new Quaternion(new float[] { 0.f, 0.f, 0.f });
 		this.slopeX = 0.f;
 		this.slopeY = 0.f;
+
+		// 计算中间变量
+		this.under_f = -1 - this.mass * Math.pow(this.radius, 2) / this.momentOfInertia;
+		this.under_m = this.momentOfInertia / this.mass + this.radius * this.radius;
 	}
 
 	public void timeEval(float tpf) {
+		// 计算受力
+		double F_x = this.g * this.slopeX - this.alpha_v * this.velocityX;
+		double F_y = this.g * this.slopeY - this.alpha_v * this.velocityY;
+		double M_x = -this.alpha_m * this.angularVelocityX;
+		double M_y = -this.alpha_m * this.angularVelocityY;
+
+		double f_x;
+		double f_y;
+
+		// 计算相对速度
+		double deltavx = -this.velocityX + this.radius * this.angularVelocityY;
+		double deltavy = -this.velocityY - this.radius * this.angularVelocityX;
+		double deltav = Math.sqrt(deltavx * deltavx + deltavy * deltavy);
+
+		// 判断是否已经发生滑动，并计算受力
+		if (deltav > 0.01) {
+			double f_max = this.miu * this.mass * this.g;
+			f_x = deltavx / deltav * f_max;
+			f_y = deltavy / deltav * f_max;
+		} else {
+			f_x = F_x / this.under_f + M_y / this.under_m;
+			f_y = F_y / this.under_f - M_x / this.under_m;
+			double f = Math.sqrt(f_x * f_x + f_y * f_y);
+			double f_max = this.miu * this.mass * this.g;
+			if (f > f_max) {
+				f_x = f_x / f * f_max;
+				f_y = f_y / f * f_max;
+			}
+		}
+
+		// 计算加速度
+		double a_x = (F_x + f_x) / this.mass;
+		double a_y = (F_y + f_y) / this.mass;
+		double beta_x = (M_x + f_y * this.radius) / this.momentOfInertia;
+		double beta_y = (M_y - f_x * this.radius) / this.momentOfInertia;
+
+		// 计算速度
+		this.velocityX += a_x * tpf;
+		this.velocityY += a_y * tpf;
+		this.angularVelocityX += beta_x * tpf;
+		this.angularVelocityY += beta_y * tpf;
+
+		// 计算位置变化
+		this.locationX += this.velocityX * tpf;
+		this.locationY += this.velocityY * tpf;
+
+		// 计算角度变化
+		Quaternion quaternion = new Quaternion();
+		double angle = tpf * Math.sqrt(this.angularVelocityX * this.angularVelocityX
+				+ this.angularVelocityY * this.angularVelocityY + this.angularVelocityZ * this.angularVelocityZ);
+		if (angle > 0.001)
+			quaternion.fromAngleAxis((float) angle,
+					new Vector3f(this.angularVelocityX, this.angularVelocityY, this.angularVelocityZ).normalize());
+		this.angular = quaternion.mult(this.angular);
+
+		//更新几何体位置
 		updateSphere();
 	}
 
