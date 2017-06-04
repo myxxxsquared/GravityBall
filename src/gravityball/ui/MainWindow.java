183484d4b3d2150ab5839389881bd300d0b55f14
NickeZXL <xlzhaopku@outlook.com>
2017-06-04 17:03:30 +0800

    Update MainWindow.java

--- src/gravityball/ui/MainWindow.java
+++ src/gravityball/ui/MainWindow.java
@@ -3,17 +3,14 @@ package gravityball.ui;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GradientPaint;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -39,11 +36,11 @@ public class MainWindow extends JFrame {
 	public LosePanel lose;
 	public WinPanel win;
 	public WinFinalPanel winFinal;
-	
+
 	boolean isInitial;
-	
+
 	int totalScore;
-	
+
 	/** 程序设置 */
 	private AppSettings settings;
 
@@ -82,7 +79,7 @@ public class MainWindow extends JFrame {
 
 		this.add(win, new GBC(0, 0).setFill(GBC.BOTH).setWeight(100, 100));
 		win.setVisible(false);
-		
+
 		this.add(winFinal, new GBC(0, 0).setFill(GBC.BOTH).setWeight(100, 100));
 		winFinal.setVisible(false);
 
@@ -127,21 +124,26 @@ public class MainWindow extends JFrame {
 		return out.toString();
 	}
 
+	public JTextArea makeDescription(String text, int font_size, Color font_color) {
+		JTextArea text_area = new JTextArea();
+		text_area.setText(text);
+		text_area.setEditable(false);
+		text_area.setBackground(null);
+		text_area.setOpaque(false);
+		text_area.setFont(new Font("Serif", Font.BOLD, font_size));
+		text_area.setForeground(font_color);
+		return text_area;
+	}
+
 	class StartPanel extends JPanel {
 		private JButton select_level;
 		private JTextArea description;
 		private JTextArea title;
 
 		// 设置背景
-		protected void paintComponent(Graphics g1) {
-			Graphics2D g = (Graphics2D) g1;
-			super.paintComponent(g);
-			int width = getWidth();
-			int height = getHeight();
-
-			GradientPaint paint = new GradientPaint(0, 0, Color.CYAN, 0, height, Color.MAGENTA);
-			g.setPaint(paint);
-			g.fillRect(0, 0, width, height);
+		protected void paintComponent(Graphics g) {
+			ImageIcon icon = new ImageIcon("assets/Background/01.jpg");
+			g.drawImage(icon.getImage(), 0, 0, getSize().width, getSize().height, this);// 图片会自动缩放
 		}
 
 		public StartPanel() {
@@ -160,21 +162,14 @@ public class MainWindow extends JFrame {
 					}
 				}
 			});
-			description = new JTextArea(100, 400);
-			description.setText(
-					"Tilt the board by moving your mouse\n" + "the board will tilt to the direction of the cursor.\n"
-							+ "Avoid the obstacles and\n" + "make the ball roll to the chequered flag.\n");
-			description.setEditable(false);
-			description.setBackground(null);
-			description.setOpaque(false);
-			description.setFont(new Font("Serif", Font.BOLD, 20));
 
-			title = new JTextArea(50, 100);
-			title.setText("GRAVITY BALL");
-			title.setEditable(false);
-			title.setBackground(null);
-			title.setOpaque(false);
-			title.setFont(new Font("Serif", Font.BOLD, 50));
+			String text = "Tilt the board by moving your mouse\n"
+					+ "the board will tilt to the direction of the cursor.\n" + "Avoid the obstacles and\n"
+					+ "make the ball roll to the chequered flag.\n";
+			description = makeDescription(text, 20, Color.gray);
+
+			text = "GRAVITY BALL";
+			title = makeDescription(text, 50, Color.gray);
 
 			// TODO
 			this.add(select_level, new GBC(0, 2, 2, 1).setAnchor(GBC.CENTER).setWeight(100, 100));
@@ -190,224 +185,102 @@ public class MainWindow extends JFrame {
 		JButton level4;
 		JButton level5;
 		JTextArea title;
+		JTextArea loading;
 
-		
-		public SelectPanel() {
-			super();
-			this.setLayout(new GridBagLayout());
-
-			// 设置组件
-			title = new JTextArea(50, 100);
-			title.setText("SELECT LEVEL");
-			title.setEditable(false);
-			title.setBackground(null);
-			title.setOpaque(false);
-			title.setFont(new Font("Serif", Font.BOLD, 35));
-
-			level1 = new JButton("LEVEL 1");
-			level1.addActionListener(new ActionListener() {
+		JButton makeButton(Integer game_level) {
+			JButton button = new JButton();
+			String str = "LEVEL " + game_level.toString();
+			button = new JButton(str);
+			button.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent event) {
 					try {
 						totalScore = 0;
-						gameLevel = 1;
-						select.setVisible(false);
+						gameLevel = game_level;
+						loading.setVisible(true);
 						
-						if(!isInitial)
+						if (!isInitial)
 							Program.mainWindow.remove(c);
 						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
 						c = ctx.getCanvas();
 						Program.mainWindow.add(c,
 								new GBC(0, 0).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(100, 100));
+						c.setVisible(false);
 						isInitial = false;
-						
+
 						scenes.enqueue(new Runnable() {
 							@Override
 							public void run() {
 								scenes.gameReset();
 							}
 						});
-						
+
 						scenes.enqueue(new Runnable() {
 							@Override
 							public void run() {
-								scenes.loadFromFile(new JSONObject(
-										readall(MainWindow.class.getResourceAsStream("/Maps/map1.json"))));
+								String str_file = "/Maps/map" + game_level.toString() + ".json";
+								scenes.loadFromFile(
+										new JSONObject(readall(MainWindow.class.getResourceAsStream(str_file))));
+
+								
 								scenes.gameStart();
+								loading.setVisible(false);
+								select.setVisible(false);
+								Program.mainWindow.c.setVisible(true);
+								game.setVisible(true);
 							}
 						});
-						game.setVisible(true);
 
 					} catch (Exception e) {
 						// System.out.println(e.getMessage());
 					}
 				}
 			});
-			
-			level2 = new JButton("LEVEL 2");
-			level2.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					try {
-						totalScore = 0;
-						gameLevel = 2;
-						select.setVisible(false);
-						if(!isInitial)
-						Program.mainWindow.remove(c);
-						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
-						c = ctx.getCanvas();
-						Program.mainWindow.add(c,
-								new GBC(0, 0).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(100, 100));
-						isInitial = false;
-						
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.gameReset();
-							}
-						});
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.loadFromFile(new JSONObject(
-										readall(MainWindow.class.getResourceAsStream("/Maps/map2.json"))));
-								scenes.gameStart();
-							}
-						});
-						game.setVisible(true);
+			return button;
+		}
 
-					} catch (Exception e) {
-						// System.out.println(e.getMessage());
-					}
-				}
-			});
-			
-			level3 = new JButton("LEVEL 3");
-			level3.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					try {
-						totalScore = 0;
-						gameLevel = 3;
-						select.setVisible(false);
+		// 设置背景
+		protected void paintComponent(Graphics g) {
+			ImageIcon icon = new ImageIcon("assets/Background/01.jpg");
+			g.drawImage(icon.getImage(), 0, 0, getSize().width, getSize().height, this);// 图片会自动缩放
+		}
 
-						if(!isInitial)
-						Program.mainWindow.remove(c);
-						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
-						c = ctx.getCanvas();
-						Program.mainWindow.add(c,
-								new GBC(0, 0).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(100, 100));
-						isInitial = false;
-						
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.gameReset();
-							}
-						});
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.loadFromFile(new JSONObject(
-										readall(MainWindow.class.getResourceAsStream("/Maps/map3.json"))));
-								scenes.gameStart();
-							}
-						});
-						game.setVisible(true);
+		public SelectPanel() {
+			super();
+			this.setLayout(new GridBagLayout());
 
-					} catch (Exception e) {
-						// System.out.println(e.getMessage());
-					}
-				}
-			});
-			
-			level4 = new JButton("LEVEL 4");
-			level4.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					try {
-						totalScore = 0;
-						gameLevel = 4;
-						select.setVisible(false);
-
-						if(!isInitial)
-						Program.mainWindow.remove(c);
-						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
-						c = ctx.getCanvas();
-						Program.mainWindow.add(c,
-								new GBC(0, 0).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(100, 100));
-						isInitial = false;
-						
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.gameReset();
-							}
-						});
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.loadFromFile(new JSONObject(
-										readall(MainWindow.class.getResourceAsStream("/Maps/map4.json"))));
-								scenes.gameStart();
-							}
-						});
-						game.setVisible(true);
-
-					} catch (Exception e) {
-						// System.out.println(e.getMessage());
-					}
-				}
-			});
-			
-			level5 = new JButton("LEVEL 5");
-			level5.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					try {
-						totalScore = 0;
-						gameLevel = 5;
-						select.setVisible(false);
-
-						if(!isInitial)
-						Program.mainWindow.remove(c);
-						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
-						c = ctx.getCanvas();
-						Program.mainWindow.add(c,
-								new GBC(0, 0).setAnchor(GBC.CENTER).setFill(GBC.BOTH).setWeight(100, 100));
-						isInitial = false;
-						
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.gameReset();
-							}
-						});
-						scenes.enqueue(new Runnable() {
-							@Override
-							public void run() {
-								scenes.loadFromFile(new JSONObject(
-										readall(MainWindow.class.getResourceAsStream("/Maps/map5.json"))));
-								scenes.gameStart();
-							}
-						});
-						game.setVisible(true);
-
-					} catch (Exception e) {
-						// System.out.println(e.getMessage());
-					}
-				}
-			});
+			// 设置组件
+			title = makeDescription("SELECT LEVEL", 35, Color.gray);
+			loading = makeDescription("LOADING...", 20, Color.gray);
+			level1 = makeButton(new Integer(1));
+			level2 = makeButton(new Integer(2));
+			level3 = makeButton(new Integer(3));
+			level4 = makeButton(new Integer(4));
+			level5 = makeButton(new Integer(5));
+			JTextArea empty = makeDescription("", 100, Color.white);
 
 			// TODO
-			this.add(title, new GBC(0, 0).setAnchor(GBC.NORTH).setWeight(100, 100));
-			this.add(level1, GBC.RELATIVE);
-			this.add(level2, GBC.RELATIVE);
-			this.add(level3, GBC.RELATIVE);
-			this.add(level4, GBC.RELATIVE);
-			this.add(level5, GBC.RELATIVE);
+			GridBagConstraints s = new GridBagConstraints();
+			s.gridwidth = 0;
+			s.anchor = GridBagConstraints.NORTH;
+			this.add(title, s);
+			loading.setVisible(false);
+			this.add(empty, s);
+			s.gridwidth = 1;
+			s.anchor = GridBagConstraints.SOUTH;
+			this.add(level1, s);
+			this.add(level2, s);
+			this.add(level3, s);
+			this.add(level4, s);
+			s.gridwidth = 0;
+			this.add(level5, s);
+			s.anchor = GridBagConstraints.NORTH;
+			this.add(loading, s);
 		}
 	}
 
 	public class GamePanel extends JPanel {
 		JButton game_pause;
 		JLabel game_score;
-		
 
 		GamePanel() {
 			game_pause = new JButton("PAUSE");
@@ -420,8 +293,8 @@ public class MainWindow extends JFrame {
 								scenes.gamePause();
 							}
 						});
-						if(!isInitial)
-						Program.mainWindow.remove(c);
+						if (!isInitial)
+							Program.mainWindow.remove(c);
 						game.setVisible(false);
 						pause.setVisible(true);
 					} catch (Exception e) {
@@ -429,7 +302,7 @@ public class MainWindow extends JFrame {
 					}
 				}
 			});
-			
+
 			game_score = new JLabel();
 
 			// TODO
@@ -442,6 +315,12 @@ public class MainWindow extends JFrame {
 		JButton game_retry;
 		JButton game_resume;
 		JButton select_level;
+
+		// 设置背景
+		protected void paintComponent(Graphics g) {
+			ImageIcon icon = new ImageIcon("assets/Background/01.jpg");
+			g.drawImage(icon.getImage(), 0, 0, getSize().width, getSize().height, this);// 图片会自动缩放
+		}
 
 		public PausePanel() {
 			game_retry = new JButton("RETRY");
@@ -464,8 +343,8 @@ public class MainWindow extends JFrame {
 								scenes.gameStart();
 							}
 						});
-						if(!isInitial)
-						Program.mainWindow.remove(c);
+						if (!isInitial)
+							Program.mainWindow.remove(c);
 						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
 						c = ctx.getCanvas();
 						Program.mainWindow.add(c,
@@ -524,6 +403,12 @@ public class MainWindow extends JFrame {
 		JButton game_retry;
 		JButton select_level;
 
+		// 设置背景
+		protected void paintComponent(Graphics g) {
+			ImageIcon icon = new ImageIcon("assets/Background/01.jpg");
+			g.drawImage(icon.getImage(), 0, 0, getSize().width, getSize().height, this);// 图片会自动缩放
+		}
+
 		public LosePanel() {
 			game_retry = new JButton("RETRY");
 			game_retry.addActionListener(new ActionListener() {
@@ -545,8 +430,8 @@ public class MainWindow extends JFrame {
 								scenes.gameStart();
 							}
 						});
-						if(!isInitial)
-						Program.mainWindow.remove(c);
+						if (!isInitial)
+							Program.mainWindow.remove(c);
 						final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
 						c = ctx.getCanvas();
 						Program.mainWindow.add(c,
@@ -563,8 +448,8 @@ public class MainWindow extends JFrame {
 			select_level.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent event) {
 					try {
-						if(!isInitial)
-						Program.mainWindow.remove(c);
+						if (!isInitial)
+							Program.mainWindow.remove(c);
 						lose.setVisible(false);
 						select.setVisible(true);
 					} catch (Exception e) {
@@ -582,6 +467,12 @@ public class MainWindow extends JFrame {
 	public class WinPanel extends JPanel {
 		JButton next_level;
 		JButton select_level;
+
+		// 设置背景
+		protected void paintComponent(Graphics g) {
+			ImageIcon icon = new ImageIcon("assets/Background/01.jpg");
+			g.drawImage(icon.getImage(), 0, 0, getSize().width, getSize().height, this);// 图片会自动缩放
+		}
 
 		public WinPanel() {
 			next_level = new JButton("NEXT LEVEL");
@@ -607,8 +498,8 @@ public class MainWindow extends JFrame {
 									scenes.gameStart();
 								}
 							});
-							if(!isInitial)
-							Program.mainWindow.remove(c);
+							if (!isInitial)
+								Program.mainWindow.remove(c);
 							final JmeCanvasContext ctx = (JmeCanvasContext) scenes.getContext();
 							c = ctx.getCanvas();
 							Program.mainWindow.add(c,
@@ -621,13 +512,13 @@ public class MainWindow extends JFrame {
 					}
 				}
 			});
-			
+
 			select_level = new JButton("SELECT LEVEL");
 			select_level.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent event) {
 					try {
-						if(!isInitial)
-						Program.mainWindow.remove(c);
+						if (!isInitial)
+							Program.mainWindow.remove(c);
 						win.setVisible(false);
 						select.setVisible(true);
 					} catch (Exception e) {
@@ -641,19 +532,20 @@ public class MainWindow extends JFrame {
 			this.add(select_level, GBC.RELATIVE);
 		}
 	}
-	
+
 	public class WinFinalPanel extends JPanel {
 		JTextArea msg;
-		
+
+		// 设置背景
+		protected void paintComponent(Graphics g) {
+			ImageIcon icon = new ImageIcon("assets/Background/01.jpg");
+			g.drawImage(icon.getImage(), 0, 0, getSize().width, getSize().height, this);// 图片会自动缩放
+		}
+
 		WinFinalPanel() {
 			this.setLayout(new GridBagLayout());
-			msg = new JTextArea(50, 100);
-			msg.setText("YOU WIN");
-			msg.setEditable(false);
-			msg.setBackground(null);
-			msg.setOpaque(false);
-			msg.setFont(new Font("Serif", Font.BOLD, 50));
-			
+			msg = makeDescription("YOU WIN", 50, Color.gray);
+
 			this.add(msg, new GBC(0, 0));
 		}
 	}
